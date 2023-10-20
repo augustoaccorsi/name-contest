159e72c0276001db83d32b6a72b4df6b8788ad03
I851169 <augusto.accorsi@sap.com>
2023-10-19 23:20:35 -0300

    init project and setup env

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  printWidth: 65,
+  quoteProps: "consistent",
+  singleQuote: true,
+  tabWidth: 4,
+  useTabs: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+};
